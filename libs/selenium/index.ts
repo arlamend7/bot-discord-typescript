3117934e230ca127c5e7b7c047ca19ebc53fcf79
arlan mendes <arlanmendes197@gmail.com>
2021-03-02 18:30:44 -0300

    adiciona função de pegar aula instantaneo

--- libs/selenium/index.ts
+++ libs/selenium/index.ts
@@ -1,12 +1,12 @@
 import { Builder, Capabilities, Locator, WebDriver, WebElement, WebElementPromise } from "selenium-webdriver";
 import * as chrome from "selenium-webdriver/chrome";
-import { path,defaultInstance } from "chromedriver";
+import { path } from "chromedriver";
+chrome.setDefaultService(new chrome.ServiceBuilder(path).build());
 
 export var browser: WebDriver;
 export var element: ElementHelper;
 
 export async function startBrowser() {
-    chrome.setDefaultService(new chrome.ServiceBuilder(path).build());
     return configure(await new Builder().withCapabilities(Capabilities.chrome()).build());
 }
 
@@ -15,23 +15,18 @@ function configure(page: WebDriver) {
     browser = page;
     return page;
 }
-export function turnOff() {
-    browser.close();
-    defaultInstance.disconnect()
-    conneted = false;
-}
 export var conneted : boolean;
 export interface ElementHelper {
     (): WebElementPromise;
     all(locator: Locator) : Promise<WebElement[]>;
 }
 var defaultTimeout = 20000;
-export function getElementOrWait(elemento: WebElement, waitTime: number = defaultTimeout) {
+export async function getElementOrWait(elemento: WebElement, waitTime: number = defaultTimeout) {
     return browser.wait(elemento.isDisplayed(), waitTime).then(() => elemento);
 }
 export function descePixels(pixel: number) {
     browser.executeScript(`window.scrollTo(0,${pixel});`);
 }
-export function WaitElementBeClickable(elemento: WebElement, waitTime: number = defaultTimeout) {
+export async function WaitElementBeClickable(elemento: WebElement, waitTime: number = defaultTimeout) {
     return browser.wait(elemento.isDisplayed() && elemento.isEnabled(), waitTime).then(() => elemento);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import { Message, Client } from "discord.js";
-import { uvv } from "selenium";
-import { startBrowser } from "../libs/selenium";
+import { Message, Client } from 'discord.js';
+import { getComands } from './global/functions';
+import { uvv } from './selenium';
 const client = new Client();
 
-startBrowser();
-client.once("ready", () => {
-    console.log("Ready!");
+client.once('ready', () => {
+  console.log('Ready!');
 });
-
-client.login("NzQzODg3MTI1OTc5ODU2OTMw.XzbM3w.c8x7TQpUaJHrKW10vMW7owAtQIs");
-client.on("message", (message: Message) => {
-    if(message.content.startsWith("logar uvv")){
-        uvv.logar().then(() => {
-            console.log("abriu");
-            
-        })
-    }
+client.login('NzQzODg3MTI1OTc5ODU2OTMw.XzbM3w.c8x7TQpUaJHrKW10vMW7owAtQIs');
+client.on('message', (message: Message) => {
+  if (message.content.startsWith('pegar aulas')) {
+    const [, , matricula, senha,conf] = getComands(message);
+    uvv.pegarAulas(matricula, senha,conf, message).then((aulas) => {
+      aulas.map((aula) => uvv.print(aula)).forEach((aula) => message.channel.send(aula));
+    });
+  }
 });
