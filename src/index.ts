54e9f5aa48ebc7bf28265c5caf66e94cec917621
arlan mendes <arlanmendes197@gmail.com>
2021-03-02 16:09:22 -0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Message, Client } from "discord.js";
+
+
+const client = new Client();
+
+client.once("ready", () => {
+    console.log("Ready!");
+    
+});
+
+client.login("NzQzODg3MTI1OTc5ODU2OTMw.XzbM3w.c8x7TQpUaJHrKW10vMW7owAtQIs");
+client.on("message", (message: Message) => {
+    console.log(message.content);
+});
