33068375e48b3fe004db253b26c76b019bf9debe
arlan mendes <arlanmendes197@gmail.com>
2021-03-05 08:57:49 -0300

    ajusta path

--- src/global/selenium/index.ts
+++ src/global/selenium/index.ts
@@ -1,10 +1,20 @@
-import { Builder, Capabilities } from "selenium-webdriver";
+import { Builder } from "selenium-webdriver";
 import { startBrowser } from "../../../libs/selenium";
 import * as chrome from "selenium-webdriver/chrome";
-import { path } from "chromedriver";
+import "chromedriver";
 
-chrome.setDefaultService(new chrome.ServiceBuilder(path).build());
+let options = new chrome.Options();
+let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+const screen = {
+    width: 1920,
+    height: 1080
+};
+options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+options.addArguments("--headless");
+options.addArguments("--disable-gpu");
+options.addArguments("--no-sandbox");
+options.windowSize(screen);
 
 export function createBrowser() {
-    return startBrowser(new Builder().withCapabilities(Capabilities.chrome()).build());
+    return startBrowser(new Builder().setChromeService(serviceBuilder).setChromeOptions(options).build());
 }
